ef574700c2cb43f5f83e3d38d15a915fb01d6ff5
Lechusin <122849236+Lechusin@users.noreply.github.com>
2024-01-22 22:20:31 -0500

    cambios para validacion

--- main.py
+++ main.py
@@ -31,20 +31,32 @@ def load_hub_layer():
 custom_objects = {'KerasLayer': load_hub_layer()}
 
 # Carga el modelo con objetos personalizados
-modelo = tf.keras.models.load_model("m2.h5", custom_objects=custom_objects)
+modelo_clasificacion = tf.keras.models.load_model("m2.h5", custom_objects=custom_objects)
 
+modelo_validacion = tf.keras.models.load_model("m2.h5", custom_objects=custom_objects)
 
 def procesar_imagen(img):
     tipos = ["Cordada", "Eliptica", "Flabelada", "Lanceolada", "Lobulada", "Obovada", "Obtusa", "Orbicular", "Ovada", "Palmeada", "Paripinnada", "Romboide", "Triangular", "Trifoliada"]
     try:
         img = np.array(img).astype(float) / 255
         img = cv2.resize(img, (224, 224))
-        prediccion = modelo.predict(img.reshape(-1, 224, 224, 3))
+        prediccion = modelo_clasificacion.predict(img.reshape(-1, 224, 224, 3))
         categoria_predicha = np.argmax(prediccion[0], axis=-1)
         nombre_categoria_predicha = tipos[int(categoria_predicha)]
         return {"backendImage": nombre_categoria_predicha, "number": str(categoria_predicha), "mensaje": "Procesamiento exitoso"}
     except Exception as e:
         raise RuntimeError(f"Error procesando la imagen: {str(e)}")
+
+def validar_imagen(img):
+    try:
+        img = np.array(img).astype(float) / 255
+        img = cv2.resize(img, (224, 224))
+        prediccion = modelo_validacion.predict(img.reshape(-1, 224, 224, 3))
+        categoria_predicha = np.argmax(prediccion[0], axis=-1)
+        return {"valor": str(categoria_predicha), "mensaje": "Procesamiento exitoso"}
+    except Exception as e:
+        raise RuntimeError(f"Error procesando la imagen: {str(e)}")
+
 
 
 @app.get("/")
@@ -76,15 +88,25 @@ async def procesar_imagen_endpoint(file: UploadFile):
 
 
 # Ruta de prueba para procesar una imagen desde una URL
-@app.post("/procesar-imagen-url")
-def procesar_imagen_url(url: str):
+@app.post("/validar-imagen")
+async def procesar_imagen_endpoint(file: UploadFile):
+    print("Entra")
     try:
-        respuesta = requests.get(url)
-        img = Image.open(BytesIO(respuesta.content))
-        return procesar_imagen(img)
-    except HTTPException as e:
-        # Capturar excepciones de validación y devolver detalles en la respuesta
-        return JSONResponse(content={"error": str(e.detail)}, status_code=e.status_code)
+        if file.content_type.startswith("image"):
+            content = await file.read()
+            img = Image.open(BytesIO(content))
+
+            # Convertir la imagen a formato RGB si no lo está
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+
+            # Redimensionar la imagen al tamaño esperado por la red neuronal
+            img = img.resize((224, 224))
+
+            return validar_imagen(img)
+        else:
+            raise HTTPException(status_code=415, detail="Tipo de archivo no admitido, se esperaba una imagen")
     except Exception as e:
-        # Capturar otras excepciones y devolver detalles en la respuesta
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        # Loguear el error para referencia y devolver un mensaje de error
+        print(f"Error al procesar la imagen: {str(e)}")
+        raise HTTPException(status_code=500, detail="Error interno al procesar la imagen")
